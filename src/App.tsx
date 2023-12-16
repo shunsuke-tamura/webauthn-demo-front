1024563fbf272f5896cc2baa21c5f25d2d9bcd62
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-17 06:43:34 +0900

    update: 第一認証作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,87 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [authInfo, setAuthInfo] = useState({
+    email: "",
+    password: "",
+  });
+
+  const signUp = () => {
+    fetch("/api/auth/signUp", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(authInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  const signIn = () => {
+    fetch("/api/auth/signIn", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(authInfo),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <div style={{ display: "flex", justifyContent: "center", gap: "15px" }}>
+          <div>
+            <p>email</p>
+            <input
+              type="text"
+              value={authInfo.email}
+              onChange={(e) =>
+                setAuthInfo((prev) => {
+                  return { ...prev, email: e.target.value };
+                })
+              }
+            />
+          </div>
+          <div>
+            <p>password</p>
+            <input
+              type="text"
+              value={authInfo.password}
+              onChange={(e) =>
+                setAuthInfo((prev) => {
+                  return { ...prev, password: e.target.value };
+                })
+              }
+            />
+          </div>
+        </div>
+        <br />
+        <button
+          type="button"
+          className="primary"
+          style={{ marginRight: "20px" }}
+          onClick={signUp}
+        >
+          SignUp
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button type="button" className="secondary" onClick={signIn}>
+          SignIn
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <br />
+      <button type="button" className="primary" style={{ marginRight: "20px" }}>
+        Register Device
+      </button>
+      <button type="button" className="secondary">
+        Two Factor Authentication
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
