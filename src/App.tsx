5f2391dba2dc456f52c150a29e7075f6aa657668
shunsuke-tamura <wwwxx4869@gmail.com>
2023-12-17 09:27:23 +0900

    update: 第二認証完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
 
+import {
+  startAuthentication,
+  startRegistration,
+} from "@simplewebauthn/browser";
+
 function App() {
   const [authInfo, setAuthInfo] = useState({
     email: "",
     password: "",
   });
+  const [message, setMessage] = useState("");
 
   const signUp = () => {
     fetch("/api/auth/signUp", {
@@ -29,6 +35,88 @@ function App() {
     })
       .then((res) => res.json())
       .then((data) => console.log(data));
+  };
+
+  const registerCredential = async () => {
+    const resp = await fetch("/api/webauthn/register-options", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let attResp;
+    try {
+      const opts = await resp.json();
+      console.log(JSON.stringify(opts, null, 2));
+
+      attResp = await startRegistration(opts);
+      console.log(JSON.stringify(attResp, null, 2));
+    } catch (error) {
+      const _error = error as Error;
+      console.error(_error);
+      setMessage(_error.message);
+      throw _error;
+    }
+
+    const verificationResp = await fetch("/api/webauthn/credential", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(attResp),
+    });
+
+    const verificationJSON = await verificationResp.json();
+    console.log(JSON.stringify(verificationJSON, null, 2));
+
+    if (verificationJSON && verificationJSON.verified) {
+      setMessage(`Authenticator registered!`);
+    } else {
+      setMessage(
+        `Oh no, something went wrong! Response: 
+        <pre>
+        ${JSON.stringify(verificationJSON)}
+        </pre>`
+      );
+    }
+  };
+
+  const authenticate = async () => {
+    const resp = await fetch("/api/webauthn/authentication-options");
+
+    let asseResp;
+    try {
+      const opts = await resp.json();
+      console.log(JSON.stringify(opts, null, 2));
+
+      asseResp = await startAuthentication(opts);
+      console.log(JSON.stringify(asseResp, null, 2));
+    } catch (error) {
+      const _error = error as Error;
+      console.error(_error);
+      setMessage(_error.message);
+      throw _error;
+    }
+
+    const verificationResp = await fetch("/api/webauthn/authentication", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(asseResp),
+    });
+
+    const verificationJSON = await verificationResp.json();
+    console.log(JSON.stringify(verificationJSON, null, 2));
+
+    if (verificationJSON && verificationJSON.verified) {
+      setMessage(`User authenticated!`);
+    } else {
+      setMessage(
+        `Oh no, something went wrong! Response: <pre>${JSON.stringify(
+          verificationJSON
+        )}</pre>`
+      );
+    }
   };
 
   return (
@@ -74,12 +162,22 @@ function App() {
         </button>
       </div>
       <br />
-      <button type="button" className="primary" style={{ marginRight: "20px" }}>
+      <button
+        type="button"
+        className="primary"
+        style={{ marginRight: "20px" }}
+        onClick={() => registerCredential()}
+      >
         Register Device
       </button>
-      <button type="button" className="secondary">
+      <button
+        type="button"
+        className="secondary"
+        onClick={() => authenticate()}
+      >
         Two Factor Authentication
       </button>
+      <h3>{message}</h3>
     </>
   );
 }
